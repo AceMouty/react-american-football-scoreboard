90c281f4bdfebbf3a4b4428a4a06421aab3090d0
Kyle Guerrero <kyleguerrero85@gmail.com>
2019-08-12 17:26:42 -0500

    Created a buttons functional component seperate from App

---
+++ src/buttons.js
@@ -0,0 +1,21 @@
+import React, {useState} from "react";
+import "./App.css";
+
+function Buttons (props) {
+
+	return (
+		<section className="buttons">
+			<div className="homeButtons">
+				{/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
+				<button className="homeButtons__touchdown">Home Touchdown</button>
+				<button className="homeButtons__fieldGoal">Home Field Goal</button>
+			</div>
+			<div className="awayButtons">
+				<button className="awayButtons__touchdown">Away Touchdown</button>
+				<button className="awayButtons__fieldGoal">Away Field Goal</button>
+			</div>
+		</section>
+	)
+}
+
+export default Buttons
